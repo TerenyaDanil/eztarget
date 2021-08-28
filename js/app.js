aa0f7e65776a70c0cc693391f3472d0312dae612
TerenyaDanil <47912455+TerenyaDanil@users.noreply.github.com>
2021-08-28 14:06:49 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -12,16 +12,25 @@ if (burger && menu) {
 
 
 
-const password = document.querySelectorAll('.password__img');
+function showPassword() {
+	const password = document.querySelectorAll('.password__img');
 
-for (let i = 0; i < password.length; i++) {
-	password[i].onclick = function () {
-		password[i].previousElementSibling.setAttribute("type", "text")
+	for (let i = 0; i < password.length; i++) {
+		password[i].addEventListener('click', () => {
+			if (password[i].previousElementSibling.getAttribute('type') === 'password') {
+				password[i].previousElementSibling.setAttribute("type", "text")
+
+			}
+			else {
+				password[i].previousElementSibling.setAttribute("type", "password")
+			}
+		})
+
+
 	}
 }
 
-
-
+showPassword()
 
 const lan = document.querySelectorAll('.lan-welcome');
 
